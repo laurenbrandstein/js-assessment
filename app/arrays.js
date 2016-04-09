df91376f7eae4b9533364107e38fb4ed70a09589
Lauren Brandstein <lauren.brandstein@equinox.com>
2016-04-08 22:00:35 -0400

    Add some methods for arrays

--- app/arrays.js
+++ app/arrays.js
@@ -3,15 +3,40 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
+      var i,
+          result = -1;
 
+      for(i = 0; i < arr.length; i++) {
+          if(arr[i] === item) {
+              result = i;
+              break;
+          }
+      }
+
+      return result;
   },
 
   sum : function(arr) {
+      var i,
+          sum = 0;
 
+      for(i = 0; i < arr.length; i++) {
+          sum += arr[i];
+      }
+
+      return sum;
   },
 
   remove : function(arr, item) {
+      var i;
 
+      for(i = 0; i < arr.length; i++) {
+          if(arr[i] === item) {
+              arr.splice(i, 1);
+          }
+      }
+
+      return arr;
   },
 
   removeWithoutCopy : function(arr, item) {
