2216eb6ccc4a3df4d0909fb5d783ffcc72d98782
Lauren Brandstein <lauren.brandstein@equinox.com>
2016-04-10 00:10:56 -0400

    Complete array methods

--- app/arrays.js
+++ app/arrays.js
@@ -40,46 +40,102 @@ exports.arraysAnswers = {
   },
 
   removeWithoutCopy : function(arr, item) {
+      var i;
 
+      for(i = 0; i < arr.length; i++) {
+          if(arr[i] === item) {
+              arr.splice(i, 1);
+              i -= 1;
+          }
+      }
+
+      return arr;
   },
 
   append : function(arr, item) {
-
+      arr.push(item);
+      
+      return arr;
   },
 
   truncate : function(arr) {
-
+      arr.pop();
+      
+      return arr;
   },
 
   prepend : function(arr, item) {
-
+      arr.splice(0, 0, item);
+      
+      return arr;
   },
 
   curtail : function(arr) {
-
+      arr.shift();
+      
+      return arr;
   },
 
   concat : function(arr1, arr2) {
-
+      return arr1.concat(arr2);
   },
 
   insert : function(arr, item, index) {
-
+      arr.splice(index, 0, item);
+      
+      return arr;
   },
 
   count : function(arr, item) {
+      var count = 0,
+          i;
 
+      for(i = 0; i < arr.length; i++) {
+          if(arr[i] === item) {
+              count += 1;
+          }
+      }
+
+      return count;
   },
 
   duplicates : function(arr) {
+      var arrDuplicates = [],
+          i;
 
+      arr.sort();
+
+      for(i = 0; i < arr.length; i++) {
+          if(arr[i] === arr[i - 1]) {
+              if(arrDuplicates[arrDuplicates.length - 1] !== arr[i]) {
+                  arrDuplicates.push(arr[i]);
+              }
+          }
+      }
+
+      return arrDuplicates;
   },
 
   square : function(arr) {
+      var i;
 
+      for(i = 0; i < arr.length; i++) {
+          arr[i] = arr[i] * arr[i];
+      }
+
+      return arr;
   },
 
   findAllOccurrences : function(arr, target) {
+      var i,
+          results = [];
 
+      for(i = 0; i < arr.length; i++) {
+          if(arr[i] === target) {
+              results.push(i);
+          }
+      }
+
+      return results;
   }
 };
