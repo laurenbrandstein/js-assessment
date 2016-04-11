da0eafb3f0150b57aa510f42c6d5784bff9728e6
Lauren Brandstein <lauren.brandstein@equinox.com>
2016-04-11 18:01:19 -0400

    Add object methods

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,22 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+      return fn.call(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+      constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
+      var i,
+          results = [];
 
+      Object.getOwnPropertyNames(obj).forEach(function(key, i) {
+        results.push(key + ': ' + obj[key]);
+      });
+
+
+      return results;
   }
 };
