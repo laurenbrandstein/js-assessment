960f73aeab9f139b3767b24f0ed1ea80f0bf7b29
Lauren Brandstein <lauren.brandstein@equinox.com>
2016-05-02 01:08:18 -0400

    Modules method

--- app/modules.js
+++ app/modules.js
@@ -2,6 +2,13 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.modulesAnswers = {
   createModule : function(str1, str2) {
-
+      var module = {
+          greeting: str1,
+          name: str2,
+          sayIt: function() {
+              return this.greeting + ', ' + this.name;
+          },          
+      };
+      return module;
   }
 };
