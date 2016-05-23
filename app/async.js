607f583127bb1195c978b5957367e2a1428e23ab
Lauren Brandstein <lauren.brandstein@equinox.com>
2016-05-23 04:04:57 -0400

    Async WIP

--- app/async.js
+++ app/async.js
@@ -2,10 +2,19 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+      return new Promise(function(resolve, reject) {
+          resolve(value);
+      });
   },
 
   manipulateRemoteData : function(url) {
-
+      var people;
+      $.getJSON(url, function(data) {
+           people = data.people.sort();
+      });
+      console.log(people);
+      return new Promise(function(resolve, reject) {
+          resolve(people);
+      });
   }
 };
